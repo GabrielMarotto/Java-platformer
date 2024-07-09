34cee493e3e4d41a18ab43e929629ce6e5683ff5
Gabriel Marotto Notebook <spacemantothemoon0@gmail.com>
2024-07-08 22:20:14 -0300

    Added LoadSave class for ease of future sprite loading

--- Java-platformer-game/src/entities/Player.java
+++ Java-platformer-game/src/entities/Player.java
@@ -5,6 +5,9 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
+
+import utils.LoadSave;
+
 import static utils.Constants.Directions.*;
 import static utils.Constants.PlayerConstants.*;
 
@@ -95,25 +98,12 @@ public class Player extends Entity {
 
     private void loadAnimations() {
 
-        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+        BufferedImage img = LoadSave.getSpriteAtlas(LoadSave.PLAYER_ATLAS);        
 
-        try {
-            BufferedImage img = ImageIO.read(is);
-
-            animations = new BufferedImage[9][6];
-        
+        animations = new BufferedImage[9][6];
             for (int j = 0; j < animations.length; j++) {
                 for (int i = 0; i < animations[j].length; i++) {
                     animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
-            }
-        }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
     }
