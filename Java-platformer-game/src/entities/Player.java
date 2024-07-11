6d87f097041b46546f3ac55d6b534b503781c91b
Gabriel Marotto Caxias <marottogabriel@gmail.com>
2024-07-10 22:20:01 -0300

    Added hitbox and collision logic

--- Java-platformer-game/src/entities/Player.java
+++ Java-platformer-game/src/entities/Player.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
 
+import main.Game;
 import utils.LoadSave;
 
 import static utils.Constants.Directions.*;
 import static utils.Constants.PlayerConstants.*;
-
+import static utils.HelpMethods.canMoveHere;
 
 public class Player extends Entity {
 
@@ -20,10 +21,13 @@ public class Player extends Entity {
     private boolean moving = false, attacking = false;
     private boolean left, up, right, down;
     private float playerSpeed = 2.0f;
+    private int[][] lvlData;
+    private float xDrawOffset = 21 * Game.SCALE, yDrawOffset = 4 * Game.SCALE;
 
     public Player(float x, float y, int width, int height) {
         super(x, y, width, height);
         loadAnimations();
+        initHitbox(x, y, 20*Game.SCALE, 28*Game.SCALE);
     }
 
     public void update() {
@@ -34,7 +38,8 @@ public class Player extends Entity {
     }
 
     public void render(Graphics g) {
-        g.drawImage(animations[playerAction][aniIndex], (int) x,(int) y, width, height, null);
+        g.drawImage(animations[playerAction][aniIndex], (int)(hitbox.x - xDrawOffset),(int)(hitbox.y - yDrawOffset), width, height, null);
+        drawHitbox(g);
 
     }
 
@@ -78,19 +83,32 @@ public class Player extends Entity {
 
         moving = false;
 
+        if (!left && !right && !up && !down)
+            return;
+
+        float xSpeed = 0, ySpeed = 0;
+
         if (left &&!right) { 
-            x-=playerSpeed;
-            moving = true;
+            xSpeed = -playerSpeed;
         } else if (right && !left) {
-           x+= playerSpeed; 
-            moving = true;
+            xSpeed = playerSpeed;
         } 
 
         if (up && !down) {
-            y -=playerSpeed;
-            moving = true;
+            ySpeed = -playerSpeed;
         } else if (down && !up) {
-            y +=playerSpeed;
+            ySpeed = playerSpeed;
+        }
+        
+/*         if (canMoveHere(x+xSpeed, y+ySpeed, width, height, lvlData)) {
+            this.x +=xSpeed;
+            this.y +=ySpeed;
+            moving = true;
+        } */
+
+        if (canMoveHere(hitbox.x+xSpeed, hitbox.y+ySpeed, hitbox.width, hitbox.height, lvlData)) {
+            hitbox.x +=xSpeed;
+            hitbox.y +=ySpeed;
             moving = true;
         }
     }
@@ -106,6 +124,10 @@ public class Player extends Entity {
                     animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
             }
         }
+    }
+
+    public void loadLevelData(int[][] lvlData) {
+        this.lvlData = lvlData;
     }
 
     public void resetDirBooleans() {
--- Java-platformer-game/src/levels/LevelManager.java
+++ Java-platformer-game/src/levels/LevelManager.java
@@ -41,5 +41,9 @@ public class LevelManager {
     public void update () {
 
     }
+
+    public Level getCurrentLevel() {
+        return levelOne;
+    }
 }
  
